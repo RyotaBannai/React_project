b6620d1b1db4dfb603dff88d4c9febc5a75ae6bf
RyotaBannai <ryotala0528@gmail.com>
2020-01-28 23:08:06 +0900

    Looked around inside the react app files.

--- quick-react/src/App.js
+++ quick-react/src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// 以前のバーションだとComponent classから継承（extends）してコンポーネントを作成。現在のバージョンでは functoin <component_name> でok. 
+// つまり、import React, {Component} from 'react';の Componentは不要。
+// render method も必要ない。そのかわりdomをreturn。
+// コンポーネント名はPascal形式でhtmlタグを区別するため。
+
+// コンポーネントをeditすると、webpack-dev-server が更新を認識して、自動でリロード＆反映する。
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+				<h1 className="App-title">Reactへようこそ！</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
