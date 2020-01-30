7ab667a1fc0d5c84c139291917bbdcdc4bc5f636
RyotaBannai <ryotala0528@gmail.com>
2020-01-30 20:26:28 +0900

    Learnt how to declare event handler and pass event object to it.

---
+++ sokushu/quick-react/src/MyEvent.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+export default class MyEvent extends Component{	
+	
+	//constructorでバインドすれば一箇所にまとめられる.
+	/*
+	constructor(props){
+		super(props)
+		this.show = this.show.bind(this);
+	}
+	*/
+	
+	//クラスプロパティの公文を利用して、イベントハンドラーを定義. 
+	show = (e) => {console.log(`${this.props.greet}, ${e.target.value}!!`);}
+	
+	/*
+	show(e){
+		//this を bindしないと this.props.greatでエラーになる.
+		console.log(`${this.props.greet}, ${e.target.value}!!`);
+	}
+	*/
+	render(){
+		return (
+			<form>
+				<label htmlFor="txtName">名前：</label>
+				{/*エベントハンドラーはonEvent属性で設定.エベントハンドラーにはイベントオブジェエクトが渡される（ここではe.ただし生のオブジェクト（nativeEent）ではなく、SyntheticEventである.）*/}
+			{/*}<input id="txtName" type="text" onChange={this.show.bind(this)}/>*/}
+			
+				<input id="txtName" type="text" onChange={this.show}/>
+				
+				{/*または以下のようにアロー関数でも可能.*/}
+				{/*<input id="txtName" type="text" onChange={(e)=>this.show(e)}/>*/}
+			</form>
+		);
+	}
+}
--- sokushu/quick-react/src/index.js
+++ sokushu/quick-react/src/index.js
@@ -5,7 +5,8 @@ import './index.css';
 //import MyBook from './MyBook';
 //import MyTypes, {Member} from './MyTypes';
 //import MyTypes2 from './MyTypes2';
-import MyArticle from './MyArticle';
+//import MyArticle from './MyArticle';
+import MyEvent from './MyEvent';
 import * as serviceWorker from './serviceWorker';
 
 // <App />にhtmlで作ったdomを埋め込んでもok
@@ -55,6 +56,7 @@ const articles =[
 //component に配列articlesをmapする. mapメゾット（コールバック関数）の戻り値を利用.
 //componentを利用しない場合はReact.Fragmentにkeyを指定することもできる.
 //テンプレートに直接埋め込んでもいいし、ロジックを変数として切り出してもいい.
+/*
 const list = articles.map((article)=>
 		<MyArticle {...article} key={article.id}/>
 	);
@@ -62,28 +64,25 @@ ReactDOM.render(
 	<dl>{list}</dl>,
 	document.getElementById('root')
 );
+*/
 
 /*
 ReactDOM.render(
 	//<MyTypes prop1={new Member()}/>, 
 	<MyTypes />, //This causes error in console.
 	document.getElementById('root'));
-serviceWorker.unregister();
 */
-
 
 //オブジェクトを渡して、PropTypes.shapeで型指定をする。
 /*ReactDOM.render(
 	//<MyTypes prop1={new Member()}/>, 
 	<MyTypes2 />, //This causes error in console.
 	document.getElementById('root'));
-serviceWorker.unregister();
 */
 
 /*
 ReactDOM.render(
 	<MyHello />, document.getElementById('root'));
-serviceWorker.unregister();
 */
 
 //ReactDOM.render(<App name="Bannai"/>,
@@ -93,10 +92,15 @@ ReactDOM.render(
 	<MyHello2>
 		<b>Childrenのテキスト！</b>
 	</MyHello2>, document.getElementById('root'));
-serviceWorker.unregister();
 */
+
 /*
 ReactDOM.render(
 	<MyBook info={book} />, document.getElementById('root'));
 */
+
+ReactDOM.render(
+	<MyEvent greet="Hello" />, 
+	document.getElementById('root'));
+	
 serviceWorker.unregister();
