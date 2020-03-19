43703afd2c5f55a0c2f262f70885565a0cf4dd60
RyotaBannai <ryotala0528@gmail.com>
2020-03-20 00:26:26 +0900

    type of array, hash, stringmap.

--- ts/test.js
+++ ts/test.js
@@ -11,3 +11,42 @@ var data4 = null; // any type
 // 初めて代入された型がその方になる.
 var mail = "admin@gmail.com;";
 var msg = "we are now hiring engineer. \nif you're interested in, contact us via email: \n" + mail + ". we're all looking forward hearing from Yo!";
+function show(result) {
+    return "the result is " + result + ".";
+}
+function clog(input) {
+    console.log(input);
+}
+//show(100); // error when compile to js
+clog(show(100)); // string cast
+clog(show("100")); // string cast
+var array_data = ["java", "python", "go"];
+var myarray = ["this ", "is ", "my", "fav"];
+var multi_array = [
+    //この書き方クソヤナ
+    [1, 2],
+    [3, 4]
+];
+var multi_array2 = [
+    //こっちの方がまだマシ.
+    [1, 2],
+    [3, 4]
+];
+clog(myarray[0]);
+clog(multi_array2[0]);
+var omg1 = new Array(5); //size of 5
+var omg2 = new Array(1, 2, 3); //size of 3
+// let omg3: number[] = new Array(-5); // thus this is fucking error. but why the hell compling doesn't catch? lol
+var ohyah = [];
+var obj = {
+    // インデックスシグネチャ：　何を定義しても構わない。例えば [key: string] とかでも可能.
+    hoge: "oh",
+    huga: "yah",
+    bar: "mmm. yas"
+};
+clog(obj.hoge);
+var obj2 = {
+    ohh: "yah",
+    man: "dude"
+};
+clog(obj2.ohh);
--- ts/test.ts
+++ ts/test.ts
@@ -18,3 +18,51 @@ let mail: string = "admin@gmail.com;";
 let msg = `we are now hiring engineer. 
 if you're interested in, contact us via email: 
 ${mail}. we're all looking forward hearing from Yo!`;
+
+function show(result: string) {
+  return `the result is ${result}.`;
+}
+function clog(input: any) {
+  console.log(input);
+}
+//show(100); // error when compile to js
+clog(show(<any>100)); // string cast
+clog(show("100" as any)); // string cast
+
+let array_data: string[] = ["java", "python", "go"];
+let myarray: Array<string> = ["this ", "is ", "my", "fav"];
+let multi_array: Array<Array<number>> = [
+  //この書き方クソヤナ
+  [1, 2],
+  [3, 4]
+];
+let multi_array2: number[][] = [
+  //こっちの方がまだマシ.
+  [1, 2],
+  [3, 4]
+];
+clog(myarray[0]);
+clog(multi_array2[0]);
+
+let omg1: number[] = new Array(5); //size of 5
+let omg2: number[] = new Array(1, 2, 3); //size of 3
+// let omg3: number[] = new Array(-5); // thus this is fucking error. but why the hell compling doesn't catch? lol
+let ohyah: number[] = [];
+
+let obj: { [index: string]: string } = {
+  // インデックスシグネチャ：　何を定義しても構わない。例えば [key: string] とかでも可能.
+  hoge: "oh",
+  huga: "yah",
+  bar: "mmm. yas"
+};
+clog(obj.hoge);
+
+interface StringMap {
+  [key: string]: string;
+}
+
+let obj2: StringMap = {
+  ohh: "yah",
+  man: "dude"
+};
+clog(obj2.ohh);
