ac776a6eb48bb8e417892abda3bbfa412e1fbb3f
RyotaBannai <ryotala0528@gmail.com>
2020-03-27 00:22:17 +0900

    learning vanilajs, the scope of var and let.

--- ts/src/basic_types2.ts
+++ ts/src/basic_types2.ts
@@ -12,3 +12,22 @@ let c: Color = Color.Orange;
 let d: string = Color[1]; // valueを取り出したい場合型はstring
 clog(c);
 clog(d);
+
+// void type only can be defined null or undefined.
+let unusuable: void = null;
+unusuable = undefined;
+// and actully you can use like this.
+let mynull: null = null;
+let myundefined: undefined = undefined;
+//**** */
+// when using the --strictNullChecks flag, null and undefined
+// are only assignable to any and their respective types
+
+//object is a type that represents the non-primitive type
+declare function create(o: object | null): string;
+//create(12); // this is an error
+
+//A type assertion is like a type cast in other languages
+let someValue: any = "this is a string";
+let strLength: number = (<string>someValue).length;
+clog(strLength);
