1dbd472783f6246bd0ece6a891f39e9359c00900
RyotaBannai <ryotala0528@gmail.com>
2020-06-11 13:26:21 +0900

    initialize react app

--- tweet-app/src/js/client.js
+++ tweet-app/src/js/client.js
@@ -10,7 +10,7 @@ const app = document.getElementById('app');
 /*
   トップレベルコンポーネントをRedux コンポーネントでwrap する.
   Store とReact を結びつけるためにstore をimport してProvider コンポーネントのprops として定義.
-   -> React とRedux は接続されている状態.すなわち、様々なcomponent からstore が変更されたことを検知して随時画面をレンダリングする準備ができている状態.
+   -> React とRedux は接続されている状態. すなわち、様々なcomponent からstore が変更されたことを検知して随時画面をレンダリングする準備ができている状態.
 */
 ReactDOM.render(
   <Provider store={store}>
