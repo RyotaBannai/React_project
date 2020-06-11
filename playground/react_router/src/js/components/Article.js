05386f267718ecd4bec61d50232dfad77276ffc8
RyotaBannai <ryotala0528@gmail.com>
2020-06-11 22:45:53 +0900

    refactoring... toggle item should be like this.

--- playground/react_router/src/js/components/Article.js
+++ playground/react_router/src/js/components/Article.js
@@ -5,38 +5,36 @@ export default class Article extends React.Component {
     constructor() {
         super();
         this.state = {
-            collapsed: true,
-            message: 'More Info'
+            message: 'More Info',
+            showContent: false,
         };
-        this.contentRef = React.createRef();
+        // this.contentRef = React.createRef();
     }
-    toggleContent(){
-        const node = this.contentRef.current; // get dom
-        console.log(node);
-        if(node.computedStyleMap().get('display').value === 'block')
-            node.style.display = 'none';
-        else
-            node.style.display = 'block';
+    toggleContent(event){
+        //const target = event.target; // (test: you should use this rather than refs)
+        // const node = this.contentRef.current; // get dom
+        // if(node.computedStyleMap().get('display').value === 'block')
+        //     node.style.display = 'none';
+        // else
+        //     node.style.display = 'block';
 
+        this.setState({
+           showContent: !this.state.showContent,
+        });
         this.toggleMessage();
     }
     toggleMessage(){
-        if (this.state.message === 'More Info'){
-            this.setState({
-                message: 'Hide Info'
-            })
-        }
-        else{
-            this.setState({
-                message: 'More Info'
-            })
-        }
+        if (this.state.message === 'More Info')
+            this.setState({ message: 'Hide Info',})
+        else
+            this.setState({message: 'More Info',})
     }
     render() {
         const { title, content, id, deleteArticle } = this.props;
         return <div className="col-md-4">
             <h2>{title}</h2>
-            <p ref={this.contentRef}>{content}</p>
+            {/*<p ref={this.contentRef}> {this.state.showContent ? content : null}</p>*/}
+            <p> {this.state.showContent ? content : null}</p>
             <button type='button' className='btn btn-light' onClick={this.toggleContent.bind(this)}>{this.state.message}</button>
             <a className="btn btn-default" href="#" >
                 <button type='button' className='btn btn-info'>Jump to Article</button>
