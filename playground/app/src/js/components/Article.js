110f7807f6e5c98ba1788a21fd413dfcf5962782
RyotaBannai <ryotala0528@gmail.com>
2020-06-14 17:25:23 +0900

    refactor a little bit

--- playground/app/src/js/components/Article.js
+++ playground/app/src/js/components/Article.js
@@ -4,28 +4,17 @@ import '../../sass/Article.scss';
 export default class Article extends React.Component {
     constructor() {
         super();
-        this.state = {
-            message: 'More Info',
-            showContent: false,
-        };
-
+        this.state = { showContent: false };
     }
-    toggleContent(event){
+    toggleContent = event =>
         this.setState((state, props) => ({ showContent: !state.showContent,}));
-        this.toggleMessage();
-    }
-    toggleMessage(){
-        if (this.state.message === 'More Info')
-            this.setState({ message: 'Hide Info',})
-        else
-            this.setState({ message: 'More Info',})
-    }
     render() {
         const { title, content, id, deleteArticle } = this.props;
         return <div className="col-md-4">
             <h2>{title}</h2>
             <p> {this.state.showContent ? content : null}</p>
-            <button type='button' className='btn btn-light' onClick={this.toggleContent.bind(this)}>{this.state.message}</button>
+            <button type='button' className='btn btn-light' onClick={this.toggleContent.bind(this)}>
+                {this.state.showContent ? 'Hide Info' : 'More Info'}</button>
             <a className="btn btn-default" href="#" >
                 <button type='button' className='btn btn-info'>Jump to Article</button>
             </a>
